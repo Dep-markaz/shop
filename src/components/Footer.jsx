632863ddeb4e732e69ef85ed0ccf0ae149030ebb
Dep-markaz <simple.view03@gmail.com>
2023-08-21 20:57:31 +0500

    test commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Footer = () => {
+	return <footer>All rights saved &copy;</footer>
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header>
+			<div className="">
+				<span className="logo">House Staff</span>
+				<ul className="nav">
+					<li>About</li>
+					<li>Contacts</li>
+					<li>Cabinet</li>
+				</ul>
+			</div>
+			<div className="presentation"></div>
+		</header>
+	)
+}
+
+export default Header
